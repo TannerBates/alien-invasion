6f739006e31d7bf05116281d2f151868a9b76255
Tanner Bates <tanner@Tanners-MBP.lan>
2023-04-15 17:07:42 -0500

    first commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    # a class to store all settings for alien invasion
+
+
+    def __init__(self):
+        # initalize the games settings 
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
+        # ships settings
+        self.ship_speed = 1.5
