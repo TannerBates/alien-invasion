51fdc525dca3f69374331bfcb55a030b25613a5c
Tanner Bates <tanner@Tanners-MBP.lan>
2023-04-16 18:06:24 -0500

    add drop speed for alien fleet

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings:
         # screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (260, 260, 260)
 
         # bullet settings 
         self.bullet_speed = 5.0
@@ -22,3 +22,7 @@ class Settings:
 
         # alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
