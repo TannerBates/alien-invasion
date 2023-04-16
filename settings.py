378621d4491450d60d6fdff8080f076eaaeda3ae
Tanner Bates <tanner@Tanners-MBP.lan>
2023-04-16 16:18:43 -0500

    changes to the ship and bullet speed

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # bullet settings 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -18,4 +18,4 @@ class Settings:
 
 
         # ships settings
-        self.ship_speed = 3
+        self.ship_speed = 4
