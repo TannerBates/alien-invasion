43c5e03020ea22ad2a4bb0d5fae92ccefeb8e8fa
Tanner Bates <tanner@Tanners-MBP.lan>
2023-04-16 21:56:45 -0500

    changes to the ship and bullet speed

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.bg_color = (260, 260, 260)
 
         # bullet settings 
-        self.bullet_speed = 5.0
+        self.bullet_speed = 8.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -23,7 +23,7 @@ class Settings:
 
         # alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 15
 
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
